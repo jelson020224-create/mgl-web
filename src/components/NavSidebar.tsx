c3ff1a6b54e983a4c38b4f5b1292817122d40e56
jelson020224-create <jelson020224-create@users.noreply.github.com>
2026-07-01 09:15:21 +0800

    Remove Approach admin page

--- src/components/NavSidebar.tsx
+++ src/components/NavSidebar.tsx
@@ -143,14 +143,6 @@ export default function NavSidebar({ pathname, settings, children }: NavSidebarP
         )}
       </div>
 
-      {/* Approach */}
-      <NavLink href="/admin/approach" pathname={pathname}>
-        <svg className="w-4 h-4 shrink-0 opacity-60" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth={1.5}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
-        </svg>
-        Approach
-      </NavLink>
-
       {/* Messages */}
       <NavLink href="/admin/messages" pathname={pathname}>
         <svg className="w-4 h-4 shrink-0 opacity-60" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth={1.5}>
